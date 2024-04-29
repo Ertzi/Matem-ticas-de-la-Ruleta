702f936b8f5e1f56395e7837642977b53821696d
Julen Ercibengoa Portatil <julenertzisa@gmail.com>
2024-04-29 19:20:41 +0200

    oinarrizko kodigoa igo

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+
+class Ruleta():
+    """
+    Clase para simular la ruleta, con diferentes modos de juego:
+    - Color: Apuestas todo a un color
+    - ...
+    - ...
+    """
+    def __init__(self, modo_de_juego):
+        pass
