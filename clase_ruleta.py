f5d7eee6b01039e5b30698190ba2109a3ddfc472
Julen Ercibengoa Portatil <julenertzisa@gmail.com>
2024-05-11 20:32:41 +0200

    act

--- clase_ruleta.py
+++ clase_ruleta.py
@@ -4,28 +4,15 @@ class Ruleta():
     """
     Clase para simular la ruleta, con diferentes modos de juego:
     - 'color': apuestas todo a un color
-    - 'par_impar': apuestas todo a que salga par o impar
-    - 'pasa_falta': pasa = 1-18, falta = 19-36
-    - 'docena': apuestas todo a que salga una docena de las tres que hay (1-12, 13-24, 25-36)
-    - 'dos_docenas': lo mismo pero a dos de las tres docenas
-    - 'columnas': se apuesta a una de las tres columnas que hay
-    - 'dos_columnas': Se apuesta a dos de las tres columnas que hay
-    - 'seisena': 
-    - 'cuadro':
-    - 'transversal': 
-    - 'caballo': 
-    - 'pleno': 
     """
-    def __init__(self, modo_de_juego):
-        self.modo_de_juego = modo_de_juego
-
+    def __init__(self):
         self.__negro = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
         self.__rojo =  [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
 
     def __conseguir_valor(self):
         return random.randint(0,36)
     
-    def __apostar_color(self, cantidad, color): # __ para no poder acceder a la funcion desde fuera de la clase
+    def apostar(self, cantidad, color = "negro"): # __ para no poder acceder a la funcion desde fuera de la clase
         valor = self.__conseguir_valor()
         if valor in self.__negro and color == "negro":
             return cantidad * 2
@@ -33,24 +20,6 @@ class Ruleta():
             return cantidad * 2
         else:
             return 0
-    
-    def __apostar_par_impar(self, cantidad, paridad):
-        valor = self.__conseguir_valor()
-        if valor != 0:
-            if valor % 2 == 0 and paridad == "par":
-                return cantidad * 2
-            elif valor % 2 != 0 and paridad == "impar":
-                return cantidad * 2
-            else:
-                return 0
-        else:
-            return 0
-    
-    def apostar(self, cantidad, color = "negro", paridad = "par"):
-        if self.modo_de_juego == "color":
-            return self.__apostar_color(cantidad, color)
-        
-        elif self.modo_de_juego == "par_impar":
-            return self.__apostar_par_impar(cantidad, paridad)
+
 
         
--- partidas.py
+++ partidas.py
@@ -2,21 +2,64 @@ from clase_ruleta import Ruleta
 import numpy as np
 import matplotlib.pyplot as plt
 
+ruleta = Ruleta()
 
-T = 5000
-N = 1000
-apuestas_color = Ruleta("color")
-historial = np.zeros(T)
+def hay_dinero_suficiento(dinero,apuesta):
+    return dinero >= apuesta
+
+def simulacion_turnos(turnos, dinero_inicial, dinero_minimo_apuesta, graficar = True):
+    se_ha_perdido = False
+    historial_dinero = np.zeros(turnos)
+    dinero = dinero_inicial # Dinero al inicio 
+    historial_dinero[0] = dinero
+
+    # Apostar:
+    apuesta = dinero_minimo_apuesta
+    dinero -= apuesta
+
+    for i in range(1, turnos):
+        ganancias = ruleta.apostar(apuesta)
+        if ganancias == 0 and hay_dinero_suficiento(dinero, apuesta * 2): 
+            apuesta = apuesta * 2
+            dinero -= apuesta
+        elif ganancias == 0 and not hay_dinero_suficiento(dinero, apuesta * 2):
+            historial_dinero[i:] = dinero
+            se_ha_perdido = True
+            break
+        elif ganancias > 0:
+            dinero += ganancias
+            apuesta = dinero_minimo_apuesta
+            dinero -= apuesta
+        historial_dinero[i] = dinero
+
+    if graficar:
+        plt.plot(historial_dinero)
+        plt.ylim([0,np.max(historial_dinero) + 10])
+        plt.show()
+
+    return dinero, se_ha_perdido
 
 
-for i in range(T):
-    if N < 10:
+
+# simulacion_turnos(100,1000,10,True)
+
+
+dinero_banco = 1000000
+numero_de_accesos_casino = 50
+hist_dinero = np.zeros(numero_de_accesos_casino)
+for i in range(numero_de_accesos_casino):
+    if dinero_banco >= 1000:
+        dinero_turno = 10000
+        dinero_banco-= dinero_turno
+        dinero, se_ha_perdido = simulacion_turnos(100, dinero_turno, 10, False)
+        dinero_banco += dinero
+    else:
+        print("Se ha perdido el dinero")
+        hist_dinero[i:] = dinero_banco
         break
-    N = N - 10
-    apuesta = 10
-    N += apuestas_color.apostar(apuesta, color = "negro")
-    historial[i] = N
+    hist_dinero[i] = dinero_banco
 
-plt.plot(historial)
-plt.ylim([0,np.max(historial) + 10])
+plt.plot(hist_dinero)
+plt.ylim([np.min(hist_dinero)-10,np.max(hist_dinero) + 10])
 plt.show()
+
